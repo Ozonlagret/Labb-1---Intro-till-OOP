aae80b016634d49ccd708b55b45f95db3e095e3a
Ozonlagret <tim.drejhede@gmail.com>
2024-10-27 17:13:58 +0100

    Add project files.

---
+++ Labb 1 - Intro till OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Labb_1___Intro_till_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle();
+
+            circle1.Radius = 5;
+
+            float area = circle1.GetArea(circle1.Radius, circle1._pi);
+
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ Labb 1 - Intro till OOP/circle.cs
@@ -0,0 +1,13 @@
+﻿namespace Labb_1___Intro_till_OOP
+{
+    public class Circle
+    {
+        public float _pi = 3.141f;
+        public int Radius;
+
+        public float GetArea(int Radius, float _pi)
+        {
+            return Radius * Radius * _pi;
+        }
+    }
+}
